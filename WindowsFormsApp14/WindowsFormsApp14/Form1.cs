03a1a2d42b0964ad4b907b45918ed862ccf7b05c
THEDERP655129 <83438557+THEDERP655129@users.noreply.github.com>
2021-07-17 14:11:56 -0700

    Add files via upload
    this is a place where I am going to back this up because ms defender is annoying

---
+++ WindowsFormsApp14/WindowsFormsApp14/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp14
+{
+    public partial class Form1 : Form
+    {
+        
+        Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int ssw = Screen.PrimaryScreen.Bounds.Width;
+            int ssh = Screen.PrimaryScreen.Bounds.Height;
+
+            int xpos = rnd.Next(1, ssw);
+            int ypos = rnd.Next(1, ssh);
+            int xsize = rnd.Next(1, 500);
+            int ysize = rnd.Next(1, 500);
+            TopMost = true;
+
+            Bitmap ps = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics g = Graphics.FromImage(ps as Image);
+            g.CopyFromScreen(xsize, ysize, 0, 0, ps.Size);
+            //printing the screen
+            pictureBox1.Image = ps;
+
+            this.Width = xsize;
+            this.Height = ysize;
+            this.Location = new Point(xpos, ypos);
+            pictureBox1.Width = xpos;
+            pictureBox1.Height = ypos;
+            Opacity = 100;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.ShowDialog();
+        }
+    }
+}
---
+++ WindowsFormsApp14/WindowsFormsApp14/Form2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp14
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            int ssw = Screen.PrimaryScreen.Bounds.Width;
+            int ssh = Screen.PrimaryScreen.Bounds.Height;
+
+            int xpos = rnd.Next(1, ssw);
+            int ypos = rnd.Next(1, ssh);
+            int xsize = rnd.Next(1, 500);
+            int ysize = rnd.Next(1, 500);
+            TopMost = true;
+
+            Bitmap ps = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics g = Graphics.FromImage(ps as Image);
+            g.CopyFromScreen(xsize, ysize, 0, 0, ps.Size);
+            //printing the screen
+            pictureBox1.Image = ps;
+
+            this.Width = xsize;
+            this.Height = ysize;
+            this.Location = new Point(xpos, ypos);
+            pictureBox1.Width = xpos;
+            pictureBox1.Height = ypos;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
